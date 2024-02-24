0cb95a9b6fab95f38e55db3d605726cf2376997a
Ali kemal çakıral <107760881+mavrosispira@users.noreply.github.com>
2024-02-24 21:53:33 +0300

    ali

---
+++ src/Gun03/_01_OpeningWebsite.java
@@ -0,0 +1,21 @@
+package Gun03;
+
+import Utlity.MyFunc;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _01_OpeningWebsite {
+    public static void main(String[] args) {
+
+        WebDriver driver=new ChromeDriver();  // driver tarayıcı
+                       //new FirefoxDriver();
+                       //new EdgeDriver();
+
+        driver.get("https://techno.study/tr");  // web sayfasını aç
+
+
+        MyFunc.Bekle(3);
+        //bekleme
+        driver.quit();
+    }
+}
